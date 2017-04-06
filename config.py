3fb8b9448fcf2b866e31f8404fdb68e31c895826
Satheesh Valluru <vallurusateesh2009@gmail.com>
2017-04-06 11:26:24 +0530

    Flask Tutorial Code

---
+++ config.py
@@ -0,0 +1,36 @@
+# config.py
+
+class Config(object):
+    """
+    Common configurations
+    """
+
+    # Put any configurations here that are common across all environments
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+
+    TESTING = True
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
